96d28a02b559569b8aaecf73827523967416c293
kataridivya723 <Kataridivya723@gmail.com>
2026-01-23 12:08:05 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_DemoTwo.java
@@ -0,0 +1,12 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class Activity4_DemoTwo
+{
+	@Test
+    public void TestCase() 
+	{
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
