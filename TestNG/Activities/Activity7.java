548ba4699c5ce1d84b89d1e56febf824502a3d08
kataridivya723 <Kataridivya723@gmail.com>
2026-01-23 16:21:32 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity7.java
@@ -0,0 +1,67 @@
+package TestNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Activity7
+{
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	@BeforeClass
+	public void setUp() 
+	{
+		driver=new FirefoxDriver();
+		wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("https://training-support.net/webelements/login-form");	
+	}
+	@DataProvider(name="Authentication")
+	public static Object[][] creds()
+	{
+		return new Object[][]
+		{
+			 { "admin", "password", "Welcome Back, Admin!" },
+             { "admin1", "password1", "Invalid Credentials" },
+             { "wrongAdmin", "wrongPassword", "Invalid Credentials" }
+		};
+		
+	}
+	
+	
+	@Test(dataProvider = "Authentication")
+    public void loginTest(String username, String password, String expectedMessage) {
+        // Find the input fields and the login button
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Submit']"));
+ 
+        // Clear the input fields
+        usernameField.clear();
+        passwordField.clear();
+        // Enter the credentials and click Log in
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+ 
+        // Assert login message
+        String loginMessage = driver.findElement(By.cssSelector("h2.text-center")).getText();
+        Assert.assertEquals(loginMessage, expectedMessage);
+    }
+	 	@AfterClass
+	    public void tearDown() {
+	        // Close the browser
+	        driver.quit();
+	    }
+	
+
+}
