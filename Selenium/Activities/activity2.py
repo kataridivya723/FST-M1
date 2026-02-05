d1102391685b70c78a7b02e398d3e8e03a42f530
kataridivya723 <Kataridivya723@gmail.com>
2026-02-05 22:28:29 +0530

    Add files via upload

---
+++ Selenium/Activities/activity2.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Firefox() as driver:
+    driver.get("https://training-support.net/webelements/login-form")
+    print("Title of the page is " +driver.title)
+
+    driver.find_element(By.ID, "username").send_keys("admin")
+    driver.find_element(By.ID, "password").send_keys("password")
+    driver.find_element(By.CLASS_NAME, "svelte-1pdjkmx").click
+
+    message = driver.find_element(By.TAG_NAME, "h1").text
+    print(message)
---
+++ Selenium/Activities/activity3.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Firefox() as driver:
+    driver.get("https://training-support.net/webelements/login-form")
+    print("Page title is: ", driver.title)
+
+    # Find the username field
+    username = driver.find_element(By.XPATH, "//input[@id='username']")
+    # Find the password field
+    password = driver.find_element(By.XPATH, "//input[@id='password']")
+
+    username.send_keys("admin")
+    password.send_keys("password")
+
+    # Find the login button
+    login = driver.find_element(By.XPATH, "//button[text()='Submit']")
+    login.click()
+
+    # Print the login message
+    message = driver.find_element(By.XPATH, "//h1[contains(@class, 'text-center')]")
+    print("Login message: ", message.text)
+
+    driver.quit()
